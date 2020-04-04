1e07e391a4f170ebda01a77907daf7bb59e561ef
sai-pher <arimanwd@gmail.com>
2020-04-04 17:50:20 +0000

    First Commit
    Update: Created Expense class data model
    
    TODO: Create singleton SQLite DB handler
    TODO: Create homepage data analytics view (Mock)
    TODO: Create form page

---
+++ lib/db_handler/expense_model.dart
@@ -0,0 +1,66 @@
+final String tableExpense = 'expenses';
+final String columnID = '_id';
+final String columnItem = 'item';
+final String columnCategory = 'category';
+final String columnCost = 'cost';
+final String columnDate = 'date';
+
+class Expense {
+  int _id;
+  String _item;
+  String _category; //TODO: make enum at some point
+  double _cost;
+  DateTime
+      _date; //TODO: use [date.millisecondsSinceEpoch] to store in db and [DateTime.fromMillisecondsSinceEpoch] to convert back
+
+  // TODO: remember to take date as a DateTime.now()
+  // Constructor
+  Expense(this._item, this._category, this._cost, this._date);
+
+  //constructor to create expense from map object
+  Expense.fromMap(Map<String, dynamic> map) {
+    _id = map[columnID];
+    _item = map[columnItem];
+    _category = map[columnCategory];
+    _cost = map[columnCost];
+    dateMilli = map[columnDate];
+  }
+
+  // Method to create map from expense object
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnItem: item,
+      columnCategory: category,
+      columnCost: cost,
+      columnDate: dateMilli
+    };
+    if (id != null) {
+      map[columnID] = id;
+    }
+
+    return map;
+  }
+
+  // Getters and setters
+  // ignore: unnecessary_getters_setters
+  DateTime get date => _date;
+
+  int get dateMilli => _date.millisecondsSinceEpoch;
+
+  // ignore: unnecessary_getters_setters
+  set date(DateTime value) {
+    _date = value;
+  }
+
+  set dateMilli(int milliseconds) {
+    _date = DateTime.fromMillisecondsSinceEpoch(milliseconds);
+  }
+
+  double get cost => _cost;
+
+  String get category => _category;
+
+  String get item => _item;
+
+  int get id => _id;
+}
